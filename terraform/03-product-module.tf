96d51f9785113129dc00526319de54f877c1c25d
Ray Kao <raykao@gmail.com>
2023-04-04 23:54:46 -0400

    update: module renaming

--- terraform/03-product-module.tf
+++ terraform/03-product-module.tf
@@ -1,9 +1,12 @@
-module "product-1" {
-    source = "./product-1"
+module "product" {
+    source = "./product"
 
     prefix = var.prefix
     location = var.location
     nginx_jwt_path = var.nginx_jwt_path
     auto_approval_subscription_ids              = var.auto_approval_subscription_ids
     visibility_subscription_ids                 = var.visibility_subscription_ids
+    admin_username = var.admin_username
+    ssh_public_key_path = var.ssh_public_key_path
+    myip = local.myip
 }
