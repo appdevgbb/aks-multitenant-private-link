2062afb25e1650cb9dc2178720e4905b46bf86f2
Ray Kao <raykao@gmail.com>
2023-04-04 23:53:35 -0400

    add: jumpbox for network testing

--- terraform/product-1/azurerm_virtual_network.tf
+++ terraform/product-1/azurerm_virtual_network.tf
@@ -42,8 +42,69 @@ resource "azurerm_subnet" "aks" {
     name = "AksSubnet"
     resource_group_name = azurerm_resource_group.default.name
     virtual_network_name = azurerm_virtual_network.default.name
+
+    private_link_service_network_policies_enabled = false
     
     address_prefixes = [
         "10.0.3.0/24"
     ]
+}
+
+resource "azurerm_subnet" "jumpbox" {
+    name = "JumpboxSubnet"
+    resource_group_name = azurerm_resource_group.default.name
+    virtual_network_name = azurerm_virtual_network.default.name
+    
+    address_prefixes = [
+        "10.0.4.0/24"
+    ]
+}
+
+
+resource "azurerm_network_security_group" "default" {
+  
+	name                = "default-nsg"
+  	location            = azurerm_resource_group.default.location
+  	resource_group_name = azurerm_resource_group.default.name
+
+  	security_rule {
+		name                       = "AllowSSHInbound22"
+		priority                   = 100
+		direction                  = "Inbound"
+		access                     = "Allow"
+		protocol                   = "Tcp"
+		source_port_range          = "*"
+		destination_port_range     = "22"
+		source_address_prefix      = "${local.myip}"
+		destination_address_prefix = "*"
+	}
+
+	security_rule {
+		name                       = "AllowHttpInternetInbound"
+		priority                   = 200
+		direction                  = "Inbound"
+		access                     = "Allow"
+		protocol                   = "Tcp"
+		source_port_range          = "*"
+		destination_port_range     = "80"
+		source_address_prefix      = "*"
+		destination_address_prefix = "*"
+	}
+
+	security_rule {
+		name                       = "AllowHttpsInternetInbound"
+		priority                   = 201
+		direction                  = "Inbound"
+		access                     = "Allow"
+		protocol                   = "Tcp"
+		source_port_range          = "*"
+		destination_port_range     = "443"
+		source_address_prefix      = "*"
+		destination_address_prefix = "*"
+	}
+}
+
+resource "azurerm_subnet_network_security_group_association" "default" {
+	subnet_id                                = azurerm_subnet.jumpbox.id
+	network_security_group_id 			     = azurerm_network_security_group.default.id
 }
